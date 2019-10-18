5da762df65002e177da6f17a1af7d0d266680425
juanesquintero <juaneschrome@gmail.com>
2019-10-17 19:24:53 -0500

    azure mysql server connection

--- db/db.py
+++ db/db.py
@@ -1,31 +1,9 @@
 import mysql.connector as mysql
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# _user = os.getenv("EVERGREEN_USER")
-# _password = os.getenv("EVERGREEN_PASSWORD")
-
-# user=_user,
-# password=_password,
 
 cnx = mysql.MySQLConnection(
-    host="127.0.0.1",
+    host="evergreen-db-server.mysql.database.azure.com",
     port=3306,
-    user="root",
-    password="",
+    user="evergreenadmin@evergreen-db-server",
+    password="abcd.1234",
     database="evergreen",
 )
-
-'''
-import mysql.connector as mysql
-
-cnx = mysql.MySQLConnection(
-    host="127.0.0.1",
-    port=3306,
-    user=_user,
-    password=_password,
-    database="evergreen",
-)
-'''
