9360e945d7524cac2d23bbcfa96ddcd605a89d04
juanesquintero <42153890+juanesquintero@users.noreply.github.com>
2019-10-23 17:11:34 -0500

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,17 +1,9 @@
 import mysql.connector as mysql
 
-# cnx = mysql.MySQLConnection(
-#     host="evergreen-db-server.mysql.database.azure.com",
-#     port=3306,
-#     user="evergreenadmin@evergreen-db-server",
-#     password="abcd.1234",
-#     database="evergreen",
-# )
-cnx = mysql.connector.connect(
-    user="evergreenadmin@evergreen-db-server", 
-    password="abcd.1234", 
-    host="evergreen-db-server.mysql.database.azure.com", 
-    port=3306, 
-    database="evergreen", 
-    ssl_verify_cert=False
+cnx = mysql.MySQLConnection(
+    host="evergreen-db-server.mysql.database.azure.com",
+    port=3306,
+    user="evergreenadmin@evergreen-db-server",
+    password="abcd.1234",
+    database="evergreen",
 )
